25653a57f012f7c39366354c97ea3a9c9b3376bf
Anton Gorban <66065793+Zarrokk@users.noreply.github.com>
2024-05-20 19:39:59 +0300

    Delete db directory

--- db/setup.sql
+++
@@ -1,5 +0,0 @@
-ALTER USER repl_user WITH PASSWORD '123';
-CREATE TABLE emails(id SERIAL primary key,email VARCHAR(255));
-CREATE TABLE phonenum(id SERIAL primary key,phone VARCHAR(255));
-INSERT INTO emails(email)VALUES('test@test.com');
-INSERT INTO phonenum(phone)VALUES('89001111111');
